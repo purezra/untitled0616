d6fc66ab3e01a2fa66b9defc4d17a10605b8c650
purezra <zra1020@163.com>
2022-06-16 21:04:13 +0800

    hotfix2

--- src/p1/c1.java
+++ src/p1/c1.java
@@ -8,6 +8,7 @@ public class c1 {
         System.out.println("hello git 1");
         System.out.println("hello git b");
         System.out.println("hello git a");
+        System.out.println("hello now is push test");
     }
 
 }
