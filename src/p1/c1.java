4a5bfbe24c1574ffdc8b684a41d61955f2a78a45
purezra <76714620+purezra@users.noreply.github.com>
2022-06-16 21:55:14 +0800

    Update c1.java

--- src/p1/c1.java
+++ src/p1/c1.java
@@ -11,7 +11,7 @@ public class c1 {
         System.out.println("hello now is push test");
         System.out.println("hello now is push test1");
  System.out.println("this is a test that pulls remote code to merge local repository\n");
-
+   System.out.println("github change,gitee will force updates");
 
     }
 
